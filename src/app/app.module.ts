6cee103f056c94d0ec6dd7f89a07facd08ec48f9
Khajo Elias <elias.khajo@scania.com>
2022-10-20 16:28:20 +0200

    application setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import {HeaderComponent} from "./header/header.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent , HeaderComponent],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+interface Locale {
+  label:string;
+  value:string;
+}
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  // the locales the app supports
+  locales :Locale[] = [
+    { label: 'English', value: 'en-US' },
+    { label: 'Svenska', value: 'sv-SE' },
+    { label: 'Français', value: 'fr-FR' },
+    { label: 'Español', value: 'es-ES' },
+    { label: 'Português', value: 'pt-BR' },
+  ];
+
+  // the user's locale
+  detectedLocale = 'en-US';
+
+  // the default locale
+  locale = this.locales[0].value;
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  // change locale/language at runtime
+  updateLocale(locale:Event) {
+    /*console.log('update locale', locale);
+    if (this.locales.some(l => l.value === locale)) {
+      this.locale = locale;
+    }
+    const lang = locale.substring(0, 2);
+    this.translocoService.setActiveLang(lang);*/
+  }
+
+  onLocalization(locale:string): void{
+    this.detectedLocale=locale;
   }
 
 }
